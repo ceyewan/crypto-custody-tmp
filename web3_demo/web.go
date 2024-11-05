db8f0e57b155eb50d7bbab243ec17d476ae7bd46
ceyewan <ceyewan@gmail.com>
2024-11-05 08:53:31 +0800

    web3 getBalance

--- web3_demo/web.go
+++ web3_demo/web.go
@@ -1,58 +1,53 @@
-package web3_demo
+package main
 
 import (
 	"context"
-	"crypto/ecdsa"
 	"fmt"
 	"log"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-const (
-	USDTContractAddress = "0xdac17f958d2ee523a2206206994597c13d831ec7"
-	infuraURL           = "https://sepolia.infura.io/v3/your_infura_project_id"
-)
+func getBalance(address string, client *ethclient.Client) {
+	// 指定要查询的地址
+	addr := common.HexToAddress(address)
 
-func createWallet() (*ecdsa.PrivateKey, common.Address) {
-	// 创建一个新的私钥
-	privateKey, err := crypto.GenerateKey()
+	// 获取余额
+	balance, err := client.BalanceAt(context.Background(), addr, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// 从私钥中获取公钥
-	publicKey := privateKey.Public()
-	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
-	if !ok {
-		log.Fatal("error casting public key to ECDSA")
-	}
-	// 从公钥中获取地址
-	address := crypto.PubkeyToAddress(*publicKeyECDSA)
-	return privateKey, address
+
+	// 将余额从Wei转换为Ether
+	fbalance := new(big.Float)
+	fbalance.SetString(balance.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(1e18))
+
+	fmt.Printf("账户余额: %s ETH\n", ethValue.Text('f', 18))
 }
 
-func getBalance(client *ethclient.Client, address common.Address) (*big.Int, error) {
+func main() {
+	// 连接到Sepolia测试网
+	client, err := ethclient.Dial("https://sepolia.infura.io/v3/766c230ed91a48a097e2739b966bbbf7")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 指定要查询的地址
+	address := common.HexToAddress("0x007d10c5222c2f326D0145Ab9B6148C6ED9c909d")
+
+	// 获取余额
 	balance, err := client.BalanceAt(context.Background(), address, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-func main() {
-	// 连接到以太坊客户端
-	client, err := ethclient.Dial(infuraURL)
-	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
-	}
-	// 创建钱包
-	privateKey, address := createWallet()
-	// 查询余额
-	balance, err := getBalance(client, address)
-	if err != nil {
-		log.Fatalf("Failed to get balance: %v", err)
-	}
-	fmt.Printf("Balance: %s USDT\n", balance.String())
+	// 将余额从Wei转换为Ether
+	fbalance := new(big.Float)
+	fbalance.SetString(balance.String())
+	ethValue := new(big.Float).Quo(fbalance, big.NewFloat(1e18))
+
+	fmt.Printf("账户余额: %s ETH\n", ethValue.Text('f', 18))
 }
